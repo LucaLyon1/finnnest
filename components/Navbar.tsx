c012f0ca85c3a63bfef9a2d2e081bf7876392b13
Luca Savio <savioluca2@gmail.com>
2024-06-05 22:30:46 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="h-[100px] w-[60%] m-auto flex items-center justify-between">
+            <div>logo</div>
+            <div>Pricing</div>
+            <div>Blog</div>
+            <div>Job board</div>
+            <button>Book a demo</button>
+            <button>Try for free</button>
+        </nav>
+    );
+}
+
+export default Navbar;
