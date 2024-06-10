633bec5040582b9763c58c35694afc996275fa8b
Luca Savio <savioluca2@gmail.com>
2024-06-10 20:43:06 +0200

    styled navbar a bit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,23 @@
 function Navbar() {
     return (
-        <nav className="h-[100px] w-[100%] m-auto flex items-center justify-between shadow-md">
-            <div className="">finnnest</div>
-            <div>Pricing</div>
-            <div>Blog</div>
-            <div>Job board</div>
-            <button>Book a demo</button>
-            <button>Try for free</button>
+        <nav className="h-[100px] 
+        w-[100%] 
+        m-auto 
+        flex 
+        items-center 
+        justify-between 
+        shadow-md
+        px-[16%]
+        text-center
+        ">
+            <h2 className="text-4xl flex-1">finnnest</h2>
+            <div className="flex-1">Pricing</div>
+            <div className="flex-1">Blog</div>
+            <div className="flex-1">Job board</div>
+            <div className="flex-1 flex gap-3 items-center m-auto">
+                <button className="w-36 py-3 border-2 rounded-full border-cyan-400">Book a demo</button>
+                <button className="w-36 py-3 border-2 rounded-full border-cyan-400 bg-cyan-400 text-white">Try for free</button>
+            </div>
         </nav>
     );
 }
