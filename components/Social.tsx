2cca7a031af01b188c83d7fa7d75cad24706572e
Luca Savio <savioluca2@gmail.com>
2024-06-10 21:11:20 +0200

    styled socials a bit

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,11 +1,18 @@
 function Social() {
     return (
-        <div>
-            <h1>Join 10.000 companies using skill-based hiring</h1>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+        <div className="w-full bg-[#1C1C1C] mt-20">
+            <div className="w-[80%] m-auto text-center">
+                <div className="pt-5">
+                    <h1 className="text-4xl text-white">Join 10.000 companies using skill-based hiring</h1>
+                    <h1 className="text-4xl text-white">hire with finnnest.</h1>
+                </div>
+                <div className="w-full flex justify-around mt-24">
+                    <img src="" alt="h&m" />
+                    <img src="" alt="EY" />
+                    <img src="" alt="REVOLUT" />
+                    <img src="" alt="company" />
+                </div>
+            </div>
         </div>
     );
 }
