ad0e56944bb17438e5f13d5a254ca9f10f0e96b4
Luca Savio <savioluca2@gmail.com>
2024-06-10 21:03:34 +0200

    styled hero a bit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,24 @@
 function Hero() {
     return (
-        <div>
-            <h1>Test your candidates</h1>
-            <h1>Pick the best</h1>
-            <p>
-                Our libraries of quizz specifically tailored for finance interviews
-                will enable you to quickly and easily chose the best fit for your
-                company !finnnest. lets you create some custom quizz easily to
-                emphasize on the quality you are looking for in your candidates.
-            </p>
-            <button>Book a demo</button>
-            <button>Try for free</button>
+        <div className="w-[80%] m-auto grid md:grid-cols-3 mt-20 gap-5">
+            <div className="md:col-span-2 flex flex-col gap-4">
+                <h1 className="text-6xl">Test your candidates</h1>
+                <h1 className="text-6xl">Pick the best</h1>
+                <p className="text-xl">
+                    Our libraries of quizz specifically tailored for finance interviews
+                    will enable you to quickly and easily chose the best fit for your
+                    company !
+                </p>
+                <p className="text-xl">
+                    finnnest. lets you create some custom quizz easily to
+                    emphasize on the quality you are looking for in your candidates.
+                </p>
+                <div className="flex gap-3">
+                    <button className="w-36 py-3 border-2 rounded-full border-cyan-400">Book a demo</button>
+                    <button className="w-36 py-3 border-2 rounded-full border-cyan-400 bg-cyan-400 text-white">Try for free</button>
+                </div>
+            </div>
+            <div className="m-auto w-full col-span-1">IMAGE</div>
         </div>
     );
 }
